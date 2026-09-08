34933977a65f8efe6762f85961309c07084994c2
shalu37 <jawlashalu6@gmail.com>
2026-09-08 14:35:52 +0530

    isPalindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,34 @@
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        String s = "ac3?e3c&a";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+                continue;
+            }
+            //tolowerCase is used to convert all the characters into small case
+            if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
