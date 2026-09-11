ef3fe3bb0a93a29c93a5901d27c74c1a3afeb38b
shalu37 <jawlashalu6@gmail.com>
2026-09-11 14:32:48 +0530

    container with most water

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,26 +1,20 @@
-class MostWater {
+class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-
         int left = 0;
         int right = height.length - 1;
-
         int maxArea = 0;
 
         while (left < right) {
 
             int width = right - left;
-
-            int currentHeight = Math.min(
-                    height[left],
-                    height[right]
-            );
+            int currentHeight = Math.min(height[left], height[right]);
 
             int currentArea = width * currentHeight;
 
             maxArea = Math.max(maxArea, currentArea);
 
-            // Move the shorter line
+            // Move the shorter wall
             if (height[left] < height[right]) {
                 left++;
             } else {
@@ -29,5 +23,14 @@ class MostWater {
         }
 
         return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+        System.out.println(obj.maxArea(height));
     }
 }
